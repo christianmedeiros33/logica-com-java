ef9cd5fb5597a532872d6d834d9482bdfdb977c8
Christian Janke <christianmjanke@gmail.com>
2023-07-22 08:39:03 -0300

    alteracao na logica dos operadores, estava fazendo a logica ao contrario.

--- src/main/java/IncomeTax.java
+++ src/main/java/IncomeTax.java
@@ -1,13 +1,13 @@
 public class IncomeTax {
     public static void main(String[] args) {
-        double salary = 50000D;
+        double salary = 4000.0D;
         if (salary <= 2112.00){
             System.out.println("isento");
-        } else if (salary <= 2112.01 && salary >= 2826.65){
+        } else if (salary >= 2112.01 && salary <= 2826.65){
             System.out.println("7.5% : total a pagar de IR = r$" + (salary * 0.075));
-        }  else if (salary <= 2826.66 && salary >= 3751.05){
+        }  else if (salary >= 2826.66 && salary <= 3751.05){
             System.out.println("15% : total a pagar de IR = r$" + (salary * 0.15));
-        }  else if (salary <= 3751.06  && salary >= 4664.68){
+        }  else if (salary >= 3751.06  && salary <= 4664.68){
                 System.out.println("22.5% : total a pagar de IR = r$" + (salary * 0.225));
         } else{
             System.out.println("27.5% : total a pagar de IR = r$" + (salary * 0.275));
