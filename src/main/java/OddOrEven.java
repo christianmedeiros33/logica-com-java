51e4ca0f28c53820979ffe66cd59ea48c7ef5d00
Christian Janke <christianmjanke@gmail.com>
2023-07-22 07:26:47 -0300

    reforçando condição.

---
+++ src/main/java/OddOrEven.java
@@ -0,0 +1,10 @@
+public class OddOrEven {
+    public static void main(String[] args) {
+        int number = 3;
+        if (number % 2 == 0){
+            System.out.println("is even");
+        }else {
+            System.out.println("is odd");
+        }
+    }
+}
---
+++ src/main/java/SalaryCondition.java
@@ -0,0 +1,20 @@
+public class SalaryCondition {
+    public static void main(String[] args) {
+
+        float salary = 4600.50F;
+        float result = 0F;
+        String percentage = "";
+
+        if (salary > 4500) {
+            result = salary * 0.30F;
+            percentage = "30%";
+            System.out.println(result + " : " + percentage);
+
+        } else {
+            result = salary * 0.15F;
+            percentage = "15%";
+
+            System.out.println(result + " : " + percentage);
+        }
+    }
+}
