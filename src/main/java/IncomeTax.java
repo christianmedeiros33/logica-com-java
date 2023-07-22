88601d354eb0f3a3700306ef398fef328a02ac5e
Christian Janke <christianmjanke@gmail.com>
2023-07-22 08:28:23 -0300

    aprendi sobre usar o && (e) que com ele posso por duas condicao no if mas so sera true se as duas condicao forem verdadeiras.

---
+++ src/main/java/IncomeTax.java
@@ -0,0 +1,16 @@
+public class IncomeTax {
+    public static void main(String[] args) {
+        double salary = 50000D;
+        if (salary <= 2112.00){
+            System.out.println("isento");
+        } else if (salary <= 2112.01 && salary >= 2826.65){
+            System.out.println("7.5% : total a pagar de IR = r$" + (salary * 0.075));
+        }  else if (salary <= 2826.66 && salary >= 3751.05){
+            System.out.println("15% : total a pagar de IR = r$" + (salary * 0.15));
+        }  else if (salary <= 3751.06  && salary >= 4664.68){
+                System.out.println("22.5% : total a pagar de IR = r$" + (salary * 0.225));
+        } else{
+            System.out.println("27.5% : total a pagar de IR = r$" + (salary * 0.275));
+        }
+    }
+}
--- src/main/java/OddOrEven.java
+++ src/main/java/OddOrEven.java
@@ -1,6 +1,6 @@
 public class OddOrEven {
     public static void main(String[] args) {
-        int number = 3;
+        int number = 3 ;
         if (number % 2 == 0){
             System.out.println("is even");
         }else {
