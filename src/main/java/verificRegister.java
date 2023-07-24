ac5f41fa457f46c83ec748d78e9222dc357d4723
Christian Janke <christianmjanke@gmail.com>
2023-07-24 05:48:01 -0300

    aprendi sobre || e a usar o equalsIgnoreCase para comparar string

---
+++ src/main/java/verificRegister.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class verificRegister {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digitar seu nome: ");
+        String nome = input.next();
+
+        if (nome.equalsIgnoreCase("") || nome.equalsIgnoreCase("admin") || nome.equalsIgnoreCase("administrador")) {
+            System.out.println("ERRO USUARIO PRIVADO.");
+            
+        } else {
+            System.out.println(nome + ": usuario cadastrado.");
+        }
+    }
+}
