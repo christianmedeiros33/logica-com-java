3b8d26efbc29f510f1265160f0ef0d33963d8258
Christian Janke <christianmjanke@gmail.com>
2023-07-25 06:03:24 -0300

    exercicio com while e

---
+++ src/main/java/academy/devdojo/repeatingloops/ExemploOdd.java
@@ -0,0 +1,22 @@
+package academy.devdojo.repeatingloops;
+
+import java.util.Scanner;
+
+public class ExemploOdd {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o valor desejado");
+
+        int valorFinal = entrada.nextInt();
+        int i = 0;
+
+        while (i <= valorFinal) {
+            if (i % 2 != 0) {
+                System.out.println("i = " + i);
+            }
+            i = i + 1;
+        }
+    }
+}
+
