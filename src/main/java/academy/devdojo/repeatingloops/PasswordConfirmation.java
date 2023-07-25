a04f9b935a391632e0b7840cdc0bb8c6dc713d1a
Christian Janke <christianmjanke@gmail.com>
2023-07-25 06:49:42 -0300

    exercicio de login e senha  usando  while.

---
+++ src/main/java/academy/devdojo/repeatingloops/PasswordConfirmation.java
@@ -0,0 +1,27 @@
+package academy.devdojo.repeatingloops;
+
+import java.util.Scanner;
+
+public class PasswordConfirmation {
+    public static void main(String[] args) {
+
+        final String username = "1234";
+        final String password = "1234";
+        boolean exibirTelaDeLogin = true;
+        Scanner scanner = new Scanner(System.in);
+
+        while (exibirTelaDeLogin) {
+
+            System.out.println("Digite seu login.");
+            String inputUsername = scanner.nextLine();
+            System.out.println("Digite sua senha.");
+            String inputPassword = scanner.nextLine();
+
+            if (username.equals(inputUsername) && password.equals(inputPassword)) {
+                System.out.println("acesso concedido");
+                break;
+            }
+            System.out.println("Acesso negado");
+        }
+    }
+}
