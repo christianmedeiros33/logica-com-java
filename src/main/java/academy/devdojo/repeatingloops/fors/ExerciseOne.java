bca00d45666106b4d1416f3e9e509e3d3d0009cf
Christian Janke <christianmjanke@gmail.com>
2023-07-28 06:44:49 -0300

    exercises

---
+++ src/main/java/academy/devdojo/repeatingloops/fors/ExerciseOne.java
@@ -0,0 +1,9 @@
+package academy.devdojo.repeatingloops.fors;
+
+public class ExerciseOne {
+    public static void main(String[] args) {
+        for (int i = 15; i <= 200 ; i++) {
+            System.out.println(i*i);
+        }
+    }
+}
