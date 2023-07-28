66da30a3c579f31b4f934cf17010740b6fda1dea
Christian Janke <christianmjanke@gmail.com>
2023-07-28 07:00:33 -0300

    exercises do for finalizados.

---
+++ src/main/java/academy/devdojo/repeatingloops/fors/ExeeciseTwo.java
@@ -0,0 +1,9 @@
+package academy.devdojo.repeatingloops.fors;
+
+public class ExeeciseTwo {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.println(5*i);
+        }
+    }
+}
---
+++ src/main/java/academy/devdojo/repeatingloops/fors/ExerciseThree.java
@@ -0,0 +1,13 @@
+package academy.devdojo.repeatingloops.fors;
+
+public class ExerciseThree {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Fazendo tabuada do numero: " + i);
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+
+            }
+        }
+    }
+}
